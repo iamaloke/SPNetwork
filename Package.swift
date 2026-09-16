452c4ff2ced09b666d56c13851336894bc615dae
aloksingh <iamsinghaloke@gmail.com>
2026-09-16 13:50:51 +0530

    saving changes

--- Package.swift
+++ Package.swift
@@ -6,9 +6,9 @@ import PackageDescription
 let package = Package(
     name: "SPNetwork",
     platforms: [
-        .iOS(.v13),
-        .tvOS(.v13),
-        .macOS(.v13)
+        .iOS(.v17),
+        .tvOS(.v17),
+        .macOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
